c7aa699abe899261a90321c0f04a00bdcaeef4ed
shiva <shiva@DESKTOP-2KI7HP4>
2021-03-08 18:29:41 -0800

    GridPanel,WorldPanel:IP:

--- src/_05_Pixel_Art/GridPanel.java
+++ src/_05_Pixel_Art/GridPanel.java
@@ -3,75 +3,78 @@ package _05_Pixel_Art;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
-public class GridPanel extends JPanel{
+public class GridPanel extends JPanel {
 
-    private static final long serialVersionUID = 1L;
-    private int windowWidth;
-    private int windowHeight;
-    private int pixelWidth;
-    private int pixelHeight;
-    private int rows;
-    private int cols;
+	private static final long serialVersionUID = 1L;
+	private int windowWidth;
+	private int windowHeight;
+	private int pixelWidth;
+	private int pixelHeight;
+	private int rows;
+	private int cols;
 
-    // 1. Create a 2D array of pixels. Do not initialize it yet.
-    Pixel[][] pixels;
-    private Color color;
+	// 1. Create a 2D array of pixels. Do not initialize it yet.
+	Pixel[][] pixels;
+	private Color color;
 
-    public GridPanel(int w, int h, int r, int c) {
-        this.windowWidth = w;
-        this.windowHeight = h;
-        this.rows = r;
-        this.cols = c;
+	public GridPanel(int w, int h, int r, int c) {
+		this.windowWidth = w;
+		this.windowHeight = h;
+		this.rows = r;
+		this.cols = c;
 
-        this.pixelWidth = windowWidth / cols;
-        this.pixelHeight = windowHeight / rows;
+		this.pixelWidth = windowWidth / cols;
+		this.pixelHeight = windowHeight / rows;
 
-        color = Color.BLACK;
+		color = Color.BLACK;
 
-        setPreferredSize(new Dimension(windowWidth, windowHeight));
+		setPreferredSize(new Dimension(windowWidth, windowHeight));
 
-        // 2. Initialize the pixel array using the rows and cols variables.
-        pixels = new Pixel[rows][cols];
+		// 2. Initialize the pixel array using the rows and cols variables.
+		pixels = new Pixel[rows][cols];
 
-        // 3. Iterate through the array and initialize each element to a new pixel.
- for (int i = 0; i < pixels.length; i++) {
-	
-	 for (int j = 0; j < pixels[i].length; j++) {
-		 pixels[i][j] = new Pixel(100+(j*100),windowHeight);
-	 }
-}
+		// 3. Iterate through the array and initialize each element to a new pixel.
+		for (int i = 0; i < pixels.length; i++) {
 
-    }
-
-    public void setColor(Color c) {
-        color = c;
-    }
-
-    public void clickPixel(int mouseX, int mouseY) {
-      //   5. Use the mouseX and mouseY variables to change the color
-         //   of the pixel that was clicked. *HINT* Use the pixel's dimensions.
-    	if(mouseX<pixelWidth && mouseY<pixelHeight) {
-    		for (int i = 0; i < pixels.length; i++) {
-    			setColor(Color.RED);
-    		}
-    	
-    	}
-    }
-
-    @Override
-    public void paintComponent(Graphics g) {
-        // 4. Iterate through the array.
-        //    For every pixel in the list, fill in a rectangle using the pixel's color.
-        //    Then, use drawRect to add a grid pattern to your display.
-    	for (int i = 0; i < pixels.length; i++) {
-	     for (int j = 0; j < pixels.length; j++) {
-	    	//pixels.fillRect(Color.RED);
-			g.drawRect(pixels[i][j].x,  pixels[i][j].y,pixelWidth, pixelHeight);
-		}
+			for (int j = 0; j < pixels[i].length; j++) {
+				pixels[i][j] = new Pixel(i*pixelWidth, j*pixelHeight);
+			}
 		}
 
-    }
+	}
+
+//pixel width=10
+	public void setColor(Color c) {
+		color = c;
+	}
+
+	public void clickPixel(int mouseX, int mouseY) {
+		// 5. Use the mouseX and mouseY variables to change the color
+		// of the pixel that was clicked. *HINT* Use the pixel's dimensions.
+
+		pixels[mouseX / pixelWidth][mouseY / pixelHeight].color = color;
+
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		// 4. Iterate through the array.
+		// For every pixel in the list, fill in a rectangle using the pixel's color.
+		// Then, use drawRect to add a grid pattern to your display.
+		for (int i = 0; i < pixels.length; i++) {
+			for (int j = 0; j < pixels.length; j++) {
+				g.setColor(pixels[i][j].color);
+				g.fillRect(pixels[i][j].x, pixels[i][j].y, pixelWidth, pixelHeight);
+				g.setColor(color.BLACK);
+				g.drawRect(pixels[i][j].x, pixels[i][j].y, pixelWidth, pixelHeight);
+			
+				
+			}
+		}
+
+	}
 }
--- src/_06_Conways_Game_of_Life/WorldPanel.java
+++ src/_06_Conways_Game_of_Life/WorldPanel.java
@@ -11,6 +11,8 @@ import java.util.Random;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
+import _05_Pixel_Art.Pixel;
+
 public class WorldPanel extends JPanel implements MouseListener, ActionListener {
     private static final long serialVersionUID = 1L;
     private int cellsPerRow;
@@ -19,7 +21,7 @@ public class WorldPanel extends JPanel implements MouseListener, ActionListener 
     private Timer timer;
 
     // 1. Create a 2D array of Cells. Do not initialize it.
-
+   Cell[][] cells;
 
     public WorldPanel(int w, int h, int cpr) {
         setPreferredSize(new Dimension(w, h));
@@ -28,9 +30,9 @@ public class WorldPanel extends JPanel implements MouseListener, ActionListener 
         this.cellsPerRow = cpr;
 
         // 2. Calculate the cell size.
-
+         cellSize=cellsPerRow*cellSize;
         // 3a. Initialize the cell array to the appropriate size.
-
+    
         // 3b. Iterate through the array and initialize each cell.
         //    Don't forget to consider the cell's dimensions when 
         //    passing in the location.
